100822db4be20bb6a31705a1ee8a703a4f96a14d
SudharsanC2628 <sudharsan.chinnambalam@gmail.com>
2020-11-24 13:40:10 +0530

    5 files modified, 6 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -11,8 +11,15 @@ import { CartService } from "../cart.service";
 })
 export class ProductDetailsComponent implements OnInit {
   product;
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
+  }
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
